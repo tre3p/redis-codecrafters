a049893c981f585c2645be44a32808a844844f14
tre3p <whereareyo@yahoo.com>
2024-05-10 20:01:51 +0300

    first stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class Main {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
