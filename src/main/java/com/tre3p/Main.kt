cde0160c265a276fcd222a71ab7faf48696b5c0e
tre3p <whereareyo@yahoo.com>
2024-05-10 21:30:38 +0300

    add concurent tcp server logic

--- src/main/java/com/tre3p/Main.kt
+++ src/main/java/com/tre3p/Main.kt
@@ -1,26 +1,9 @@
 package com.tre3p
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.ServerSocket
-import java.net.Socket
+import com.tre3p.server.RedisTcpServer
 
 fun main() {
-    var serverSocket: ServerSocket? = null
-    var socketClient: Socket? = null
     val port = 6379
-
-    serverSocket = ServerSocket(port)
-    serverSocket.reuseAddress = true
-    socketClient = serverSocket.accept()
-    val inputStream = socketClient.getInputStream()
-    val socketStreamBr = BufferedReader(InputStreamReader(inputStream))
-
-    var readLine: String? = socketStreamBr.readLine()
-    while (readLine != null) {
-        if (readLine.lowercase() == "ping") {
-            socketClient.getOutputStream().write("+PONG\r\n".toByteArray())
-        }
-        readLine = socketStreamBr.readLine()
-    }
+    val redisServer = RedisTcpServer(port)
+    redisServer.launchServer()
 }
---
+++ src/main/java/com/tre3p/server/RedisTcpServer.kt
@@ -0,0 +1,52 @@
+package com.tre3p.server
+
+import org.apache.logging.log4j.kotlin.Logging
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
+
+class RedisTcpServer(
+    private val port: Int
+) : Logging {
+
+    private val defaultThreadPool = Executors.newVirtualThreadPerTaskExecutor()
+    private var serverSocket: ServerSocket? = null
+
+    fun launchServer() {
+        serverSocket = ServerSocket(port).also { it.reuseAddress = true }
+        thread { launchRequestListener() }
+    }
+
+    private fun launchRequestListener() {
+        serverSocket ?: throw Exception("Redis TCP server isn't launched!")
+
+        while (!Thread.currentThread().isInterrupted) {
+            val clientSocket = serverSocket!!.accept()
+            defaultThreadPool.execute {
+                try {
+                    logger.info("Received new request, processing..")
+                    val inputStream = clientSocket.getInputStream()
+                    val outputStream = clientSocket.getOutputStream()
+                    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+
+                    var readLine = bufferedReader.readLine()
+                    while (readLine != null) {
+                        if (readLine.lowercase() == "ping") {
+                            outputStream.write("+PONG\r\n".toByteArray())
+                        }
+                        readLine = bufferedReader.readLine()
+                    }
+                    logger.info("Request processed successfully!")
+                } catch (e: Exception) {
+                    logger.error("Exception while processing request", e)
+                } finally {
+                    clientSocket.close()
+                }
+            }
+        }
+    }
+}
