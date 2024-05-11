b97c377fed08f21652abc11d7b752548f902c90d
tre3p <whereareyo@yahoo.com>
2024-05-11 11:42:14 +0300

    add handlerFunc call to redis tcp server

--- src/main/java/com/tre3p/server/RedisTcpServer.kt
+++ src/main/java/com/tre3p/server/RedisTcpServer.kt
@@ -1,16 +1,16 @@
 package com.tre3p.server
 
-import com.tre3p.resp.RESPDecoder
 import org.apache.logging.log4j.kotlin.Logging
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.InputStream
+import java.io.OutputStream
 import java.lang.Exception
 import java.net.ServerSocket
 import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 class RedisTcpServer(
-    private val port: Int
+    private val port: Int,
+    private val handlerFunc: (InputStream, OutputStream) -> (Unit)
 ) : Logging {
 
     private val defaultThreadPool = Executors.newVirtualThreadPerTaskExecutor()
@@ -30,19 +30,8 @@ class RedisTcpServer(
                 try {
                     logger.info("Received new request, processing..")
                     val inputStream = clientSocket.getInputStream()
-                    println(RESPDecoder().decode(inputStream))
                     val outputStream = clientSocket.getOutputStream()
-                    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-
-                    var readLine = bufferedReader.readLine()
-                    while (readLine != null) {
-                        println(readLine)
-                        println(RESPDecoder().decode(inputStream))
-                        if (readLine.lowercase() == "ping") {
-                            outputStream.write("+PONG\r\n".toByteArray())
-                        }
-                        readLine = bufferedReader.readLine()
-                    }
+                    handlerFunc.invoke(inputStream, outputStream)
                     logger.info("Request processed successfully!")
                 } catch (e: Exception) {
                     logger.error("Exception while processing request", e)
