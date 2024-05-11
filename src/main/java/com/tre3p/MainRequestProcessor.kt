5ceaf064badb41bd035c2e6b5387066a31c80fc7
tre3p <whereareyo@yahoo.com>
2024-05-11 16:33:52 +0300

    change logic of request processing in main request processor

--- src/main/java/com/tre3p/MainRequestProcessor.kt
+++ src/main/java/com/tre3p/MainRequestProcessor.kt
@@ -3,6 +3,7 @@ package com.tre3p
 import com.tre3p.handler.HandlerRouter
 import com.tre3p.resp.RESPDecoder
 import com.tre3p.resp.RESPEncoder
+import org.apache.logging.log4j.kotlin.Logging
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -10,15 +11,21 @@ class MainRequestProcessor(
     private val respDecoder: RESPDecoder,
     private val respEncoder: RESPEncoder,
     private val router: HandlerRouter
-) {
+): Logging {
     fun processRequest(requestInputStream: InputStream, requestOutputStream: OutputStream) {
-        val decodedRequest = respDecoder.decode(requestInputStream) as List<*>
-        val response = router.route(decodedRequest)
+        while (true) {
+            val decodedStatement = respDecoder.decode(requestInputStream)
+            if (decodedStatement == null) {
+                logger.info("All instructions are read from input stream")
+                break
+            }
+            logger.info("Got instruction: $decodedStatement")
 
-        response?.let {
-            val encodedResponse = respEncoder.encode(it)
-            requestOutputStream.write(encodedResponse)
-            requestOutputStream.close()
+            router.route(decodedStatement as List<*>).let {
+                logger.info("Encoding and sending response: $it")
+                val encodedResponse = respEncoder.encode(it)
+                requestOutputStream.write(encodedResponse)
+            }
         }
     }
 }
