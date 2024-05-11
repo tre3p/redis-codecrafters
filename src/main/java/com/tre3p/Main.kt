952906e229862120f8f8fc177e449e9d339f6d73
tre3p <whereareyo@yahoo.com>
2024-05-11 11:43:07 +0300

    build request processor

--- src/main/java/com/tre3p/Main.kt
+++ src/main/java/com/tre3p/Main.kt
@@ -1,9 +1,19 @@
 package com.tre3p
 
+import com.tre3p.handler.HandlerRouter
+import com.tre3p.resp.RESPDecoder
+import com.tre3p.resp.RESPEncoder
 import com.tre3p.server.RedisTcpServer
 
+private const val TCP_PORT = 6379
+
 fun main() {
-    val port = 6379
-    val redisServer = RedisTcpServer(port)
+    val mainRequestProcessor = MainRequestProcessor(
+        RESPDecoder(),
+        RESPEncoder(),
+        HandlerRouter()
+    )
+
+    val redisServer = RedisTcpServer(TCP_PORT, mainRequestProcessor::processRequest)
     redisServer.launchServer()
 }
