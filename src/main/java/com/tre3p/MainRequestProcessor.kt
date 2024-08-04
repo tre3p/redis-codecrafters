e433a69a6e5128b7d87d731310c1eb749944bb1b
tre3p <whereareyo@yahoo.com>
2024-08-04 11:08:37 +0300

    #2: remove useless comments

--- src/main/java/com/tre3p/MainRequestProcessor.kt
+++ src/main/java/com/tre3p/MainRequestProcessor.kt
@@ -11,25 +11,11 @@ class MainRequestProcessor(
     private val respEncoder: RESPEncoder,
     private val router: HandlerRouter,
 ) : Logging {
-    /**
-     * processRequest() should be executed in a loop since one connection can send multiple commands to server
-     */
-    tailrec fun processRequest(
-        requestInputStream: InputStream,
-        // requestOutputStream: OutputStream,
-    ): ByteArray {
+    fun processRequest(requestInputStream: InputStream): ByteArray {
         val decodedStatement = respDecoder.decode(requestInputStream) ?: return ByteArray(0)
         logger.info("Got instruction: $decodedStatement")
 
         val response = router.route(decodedStatement as List<*>)
         return respEncoder.encode(response)
-
-        /*        router.route(decodedStatement as List<*>).let {
-                    logger.info("Encoding and sending response: $it")
-                    val encodedResponse = respEncoder.encode(it)
-                    requestOutputStream.write(encodedResponse)
-                }
-
-                processRequest(requestInputStream, requestOutputStream)*/
     }
 }
